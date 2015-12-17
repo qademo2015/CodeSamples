42136e02e7a220f38b9950cf645a63637571689d
qademo2015 <qademo2015@gmail.com>
2015-12-16 21:06:08 -0800

    New code samples for Python and Ruby

--- Python/007_string_palindrome_check.py
+++ Python/007_string_palindrome_check.py
@@ -12,13 +12,15 @@ def is_palindrome_1(string):
 
 # this method uses recursion
 def is_palindrome_2(string):
+    result = True
     if len(string) == 1 or len(string) == 0:
-        return True
+        result = True
     else:
         if string[0] == string[-1]:
-            is_palindrome_2(string[1:-2])
+            is_palindrome_2(string[1:-1])
         else:
-            return False
+            result = False
+    return result
 
 
 # this method does not use recursion
@@ -34,20 +36,42 @@ def is_palindrome_3(string):
         return False
 
 
+# this method does not use recursion
+def is_palindrome_4(string):
+    result = True
+    count = len(string)
+    for i in range(0, int(count/2)):
+        if string[i] != string[count-i-1]:
+            result = False
+            break
+    return result
+
+
 def main():
     string = 'sale'
     print(is_palindrome_1(string))
     string = 'racecar'
     print(is_palindrome_1(string))
+    string = 'acca'
+    print(is_palindrome_1(string))
     string = 'sale'
     print(is_palindrome_2(string))
     string = 'racecar'
+    print(is_palindrome_2(string))
+    string = 'acca'
     print(is_palindrome_2(string))
     string = 'sale'
     print(is_palindrome_3(string))
     string = 'racecar'
     print(is_palindrome_3(string))
-
+    string = 'acca'
+    print(is_palindrome_3(string))
+    string = 'sale'
+    print(is_palindrome_4(string))
+    string = 'racecar'
+    print(is_palindrome_4(string))
+    string = 'acca'
+    print(is_palindrome_4(string))
 
 if __name__ == '__main__':
     main()
--- Ruby/009_string_each_char_occurrence.rb
+++ Ruby/009_string_each_char_occurrence.rb
@@ -1,4 +1,26 @@
 ######################################################################
 # this file contains different implementations of counting how many
-# times each character appears in string
+# times each unique character appears in string
 ######################################################################
+
+def chars_counter_1(string)
+  unique_chars_hash = {}
+  for char in string.chars.to_a.uniq
+    unique_chars_hash[char] = string.count(char)
+    string.delete(char)
+  end
+  unique_chars_hash
+end
+
+def chars_counter_2(string)
+  unique_chars_hash = {}
+  string.chars.to_a.uniq.each { |char|
+    unique_chars_hash[char] = string.count(char)
+    string.delete(char)
+  }
+  unique_chars_hash
+end
+
+string = 'Stringwithmultiplecharacters'
+puts chars_counter_1(string)
+puts chars_counter_2(string)
